9486707720122406d58258555ab531e9d0734b83
robinComa <robin.coma.public@gmail.com>
2014-02-14 22:37:13 +0100

    Documentation add right navigation style and behaviour

--- dist/doc/script/documentation.js
+++ dist/doc/script/documentation.js
@@ -1,7 +1,5 @@
 (function(){
 
-    $('#toc').affix();
-
     $('.code').each(function(el){
         var $el = $(this);
         $($el.attr('data-target')).text($el.text().replace('\n', ''));
@@ -11,5 +9,30 @@
         languages: ['javascript']
     });
     hljs.initHighlighting();
+
+    /** TOC */
+    $('#toc').affix();
+
+    // Changing menu dependent on section
+    $(window).scroll(function() {
+
+        var windowTop = Math.max($('body').scrollTop(), $('html').scrollTop());
+
+        $('section').each(function (index) {
+            if (windowTop > ($(this).position().top)){
+                $('#toc > li.active').removeClass('active');
+                $('#toc > li:eq(' + index + ')').addClass('active');
+
+                $(this).find('article').each(function(index){
+                    if (windowTop > ($(this).position().top)){
+                        $('#toc > li > ul > li.active').removeClass('active');
+                        $('#toc > li > ul > li:eq(' + index + ')').addClass('active');
+                    }
+                });
+            }
+        });
+
+    }).scroll();
+    /** END OF TOC */
 
 }());
